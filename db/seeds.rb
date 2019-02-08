ac5238834dc395523a2e8291c9fbda7172855751
MSzweda <szweda.malgorzata+github@gmail.com>
2019-02-08 17:37:58 +0100

    add card seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Card.create(text_orig: 'kot', text_localized: 'cat', locale: 'en')
+Card.create(text_orig: 'pies', text_localized: 'dog', locale: 'en')
+Card.create(text_orig: 'ryba', text_localized: 'fish', locale: 'en')
+Card.create(text_orig: 'ptak', text_localized: 'bird', locale: 'en')
+Card.create(text_orig: 'krowa', text_localized: 'cow', locale: 'en')
